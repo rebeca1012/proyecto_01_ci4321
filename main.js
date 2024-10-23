5b006ca9cafd3a689f676886242862805c1e4cad
Neonhh <thenjhu@gmail.com>
2024-10-22 22:06:15 -0400

    agrega funcion pa crear tanques

--- main.js
+++ main.js
@@ -33,36 +33,52 @@ function makeInstance(geometry, color, pos) {
 	return instance;
 }
 
+//function that creates a basic tank of the specified color in the specified position
+function makeTank(tankColor, tankBasePosition){
+	//creating a rectangular tank base
+	const baseGeometry = new THREE.BoxGeometry( 2, 1, 2);
+	//creating a platform on top for more turret range of movement
+	const platformGeometry = new THREE.CylinderGeometry(0.75, 0.75, 0.6, 16);
+	//creating a sphere for the turret base
+	const tankPivotGeometry = new THREE.SphereGeometry(0.75,32,16);
+	//creating cilinder for the turret
+	const turretGeometry = new THREE.CylinderGeometry(0.18, 0.18, 1.3, 16);
+
+	//creating respective meshes
+	const tankBase = makeInstance(baseGeometry, tankColor, tankBasePosition);
+	const tankPlatform = makeInstance(platformGeometry, tankColor, {x:0, y:0, z:0});
+	const tankPivot = makeInstance(tankPivotGeometry, tankColor, {x:0, y:0, z:0});
+	const tankTurret = makeInstance(turretGeometry,tankColor, {x: 0, y: 0, z: 0});
+
+	// putting the scenegraph (tree) together
+	tankPivot.add(tankTurret);
+	tankPivot.rotation.x = Math.PI / 2; //turret starts in horizontal position
+	tankPlatform.add(tankPivot)
+	tankBase.add(tankPlatform);
+	
+	//setting position (each position will be relative to its parent in the scenegraph)
+	tankPlatform.position.copy(new THREE.Vector3(0, 0.6, 0));
+	tankPivot.position.copy(new THREE.Vector3(0, 0.6, 0));
+	tankTurret.position.copy({x: 0, y: 0.5, z: 0});
+
+	return tankBase;
+
+}
+
 //assigning tank color and base position
 const tankColor = 0xbbeeff;
-const tankBasePosition = new THREE.Vector3(0, 0, 0);
-//creating a rectangular tank base
-const baseGeometry = new THREE.BoxGeometry( 2, 1, 2);
-//creating a platform on top for more turret range of movement
-const platformGeometry = new THREE.CylinderGeometry(0.75, 0.75, 0.6, 16);
-//creating a sphere for the turret base
-const tankPivotGeometry = new THREE.SphereGeometry(0.75,32,16);
-//creating cilinder for the turret
-const turretGeometry = new THREE.CylinderGeometry(0.18, 0.18, 1.3, 16);
+const tankBasePosition = new THREE.Vector3(0, 0, 1);
+const tank = makeTank(tankColor, tankBasePosition);
+scene.add(tank);
 
-//creating respective meshes
-const tankBase = makeInstance(baseGeometry, tankColor, tankBasePosition);
-const tankPlatform = makeInstance(platformGeometry, tankColor, {x:0, y:0, z:0});
-const tankPivot = makeInstance(tankPivotGeometry, tankColor, {x:0, y:1, z:0});
-const tankTurret = makeInstance(turretGeometry,tankColor, {x: 0, y: 0, z: 0});
+const tanks =[
+	makeTank(0x44aa88, {x: -3.5, y: 0.5, z: -3}),
+	makeTank(0x8844aa, {x: 3.5, y: 0.5, z: -3})
+]
 
-//this just sets the positions relative to other positions (adding vectors)
-tankPlatform.position.copy(tankBasePosition.clone().add(new THREE.Vector3(0, 0.6, 0)));
-tankPivot.position.copy(tankPlatform.position.clone());
-tankTurret.position.copy(tankPivot.position.clone().add({x: 0, y: 0.25, z: 0}));
-
-// putting the scenegraph (tree) together
-tankPivot.add(tankTurret);
-tankPivot.rotation.x = Math.PI / 2; //turret starts in horizontal position
-tankPlatform.add(tankPivot)
-tankBase.add(tankPlatform);
-scene.add(tankBase);
-
+for (let i = 0; i < tanks.length; i++) {
+	scene.add(tanks[i]);
+}
 /*
 const material = new THREE.MeshPhongMaterial( { color: 0xbbeeff } );
 const cube = new THREE.Mesh( geometry, material );
@@ -75,7 +91,7 @@ camera.position.y = 1;
 function render(time) {
 	time *= 0.001;  // convert time to seconds
 
-	tankBase.rotation.y = time;
+	tank.rotation.y = time;
 
 	renderer.render( scene, camera );
 
